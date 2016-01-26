66f112d56c33fb7754170a58d334e506685d4cb9
paul.le-noach@telecomnancy.net <paul@linux.home>
2016-01-26 03:26:24 +0100

    configuration de la base de données(créer base dans mysql par rapport aux valeurs de notes.txt) et de FOSBundle(identification par token)

--- Site/src/PIBundle/Controller/DefaultController.php
+++ Site/src/PIBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('PIBundle:Default:index.html.twig');
+        return $this->render('PIBundle:Default:layout.html.twig');
     }
 }
